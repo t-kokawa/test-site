7b5be7299a3ce898160c05215e648e2e67b89fb8
t-kokawa <t-kokawa@digeon.co>
2024-05-01 18:56:56 +0900

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+const img = document.querySelector("img");
+img.onclick = () => {
+    const src = img.getAttribute("src");
+    if (src === "images/camera-man.jpeg") {
+        img.setAttribute("src", "images/camera-woman.jpeg");
+    } else {
+        img.setAttribute("src", "images/camera-man.jpeg");
+    }
+}
+
+const button = document.querySelector("button");
+const head = document.querySelector("h1");
+
+function setUserName() {
+    const name = prompt("名前は？");
+    if (!name) {
+        setUserName();
+    } else {
+        localStorage.setItem("name", name);
+        head.textContent = `やあ、${name}`;
+    }
+
+}
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    const name = localStorage.getItem("name");
+    head.textContent = "やっほー";
+}
+
+button.onclick = setUserName;
